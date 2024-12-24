c25d81869d26b048a8ff7c45524e484853c72ed0
rcmiku <rcmiku@gmail.com>
2024-12-24 18:20:17 +0800

    app: Bump version to 1.2
    Signed-off-by: rcmiku <rcmiku@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.rcmiku.payload.dumper.compose"
         minSdk = 30
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.1"
+        versionCode = 12
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
