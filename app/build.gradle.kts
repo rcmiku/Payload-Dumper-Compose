a531f7690b20fe7cc42bf3587171bd98595776a5
rcmiku <rcmiku@gmail.com>
2024-12-24 22:13:01 +0800

    app: Bump version to 1.3
    Signed-off-by: rcmiku <rcmiku@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.rcmiku.payload.dumper.compose"
         minSdk = 30
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.2"
+        versionCode = 13
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
