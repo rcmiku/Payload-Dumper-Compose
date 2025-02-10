17277855253deb12f434e9c3d1234495e508a7b0
rcmiku <rcmiku@gmail.com>
2025-02-10 15:25:20 +0800

    app: Bump version to 1.5
    Signed-off-by: rcmiku <rcmiku@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.rcmiku.payload.dumper.compose"
         minSdk = 30
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.4"
+        versionCode = 15
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
